d27e605c1a967433b511a5ab82d3aeb1285d3ad4
Alttaab <alttaab@gmail.com>
2022-11-03 12:39:57 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from distutils.log import debug
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "password"
+debug = DebugToolbarExtension(app)
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+
+@app.route('/')
+def index():
+    """Show homepage"""
+
+    return """
+      <html>
+        <body>
+          <h1>I am the landing page</h1>
+        </body>
+      </html>
+      """
